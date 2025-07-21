401f695adaf78ba6195b87479588a58a1039d886
Lezyynha <leticiamaria1924@hotmail.com>
2025-07-21 17:59:06 +0000

    Compilei utilizando recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
+void moverTorre(int passo){
+            if (passo > 5) return;
+            printf("Passo %d: Direita\n", passo);
+            moverTorre(passo + 1);
+        }
+
+void moverCavalo(){
+    int cima = 0;
+
+    for(int i =1; i <= 2; i++){
+        printf("Passo %d: Cima\n", i);
+        cima++;
+        if (cima ==2){
+            int direita = 0;
+            while (direita < 1){
+                printf("Passo 3: Direita\n");
+                break;
+            }
+        }
+    }
+}
+
+void moverBispo(int passo){
+    if (passo > 5) return;
+
+    for (int i = 0; i < 1; i++){ // vertical
+        for (int j = 0; j < 1; j++) { // horizontal
+            printf("Passo %d: Cima, Direita\n", passo);
+        }
+    }
+
+    moverBispo(passo + 1);
+}
+
+void moverRainha(int passo){
+    if (passo > 8) return;
+    printf("Passo %d: Esquerda\n", passo);
+    moverRainha(passo + 1);
+}
+
 int main () {
 
     int escolha;
@@ -13,57 +53,23 @@ int main () {
 
     if(escolha == 1){
         printf("Você está jogando com a Torre!\n");
-        printf("Movimento da torre: \n");
-        //FOR - cinco casas para a direita
-
-        int i;
-        for(i = 1; i <=5; i++){
-            printf("Direita\n", i);
-        }
+        moverTorre(1);
 
     } else if(escolha == 2){
         printf("Você está jogando com o Bispo!\n");
-        printf("Movimento do bispo: \n");
-        //WHILE - cinco casas para cima e direita em diagonal
-
-        int i = 1;
-        while (i <= 5)
-        {
-        printf("Cima, direita\n", i);
-        i++;
-        }
+        moverBispo(1);
         
     } else if(escolha == 3){
         printf("Você está jogando com a Rainha!\n");
-        printf("Movimento da rainha: \n");
-        //DO-WHILE - oito casas para esquerda em todas as direções
-        
-        int i = 1;
-        do
-        {
-        printf("Esquerda\n", i);
-            i++;
-        } while (i <= 8);
+        moverRainha(1);
 
     } else if (escolha == 4){
         printf("Você está joganco com o cavalo!\n");
-        printf("Movimento do cavalo: \n");
-
-        for(int i =1; i <= 2; i++){
-            printf("Baixo\n", i);
-        }
-        
-        int j = 1;
-        while(j <= 1){
-            printf("Esquerda\n", j);
-            j++;
-        }
+        moverCavalo();
 
     } else {
         printf("Opção inválida! \n");
     }
-
-    
 
 
     return 0;
